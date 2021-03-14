252c9fb46958c41d2977c622d8a16127a2b8bef0
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2021-03-14 16:12:03 -0500

    Added new build

---
+++ src/main/java/com/spingboot/TheHunt/TheHuntApplication.java
@@ -0,0 +1,13 @@
+package com.spingboot.TheHunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHuntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spingboot/TheHunt/TheHuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingboot.TheHunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
