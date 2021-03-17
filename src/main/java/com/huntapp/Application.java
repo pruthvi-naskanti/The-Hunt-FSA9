713b84b934b2cdfbcb2e6b5ae594e90338ca4216
Yashwanth-Damera <yashwanthdamera6@gmail.com>
2021-03-17 02:07:10 -0500

    Updated Models and controllers

--- src/main/java/com/huntapp/Application.java
+++ src/main/java/com/huntapp/Application.java
@@ -3,7 +3,7 @@ package com.huntapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages= {"com.huntapp.repository","com.huntapp.controllers,com.huntapp.models"})
+@SpringBootApplication(scanBasePackages= {"com.huntapp.repository","com.huntapp.controllers","com.huntapp.models"})
 public class Application {
 
 	public static void main(String[] args) {
